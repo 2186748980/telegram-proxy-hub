437df81ab821c9fe809a5fe001ff7ff7f2153e8c
wanfeng <z2186748980@gmail.com>
2026-08-24 09:36:25 +0800

    refactor: improve node type model

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,21 @@
+export type NodeType =
+  | 'socks5'
+  | 'mtproto'
+  | 'ss'
+  | 'vmess'
+  | 'vless'
+  | 'trojan'
+  | 'hysteria2'
+  | 'unknown';
+
 export interface NodeInfo {
   name: string;
-  type: string;
+  type: NodeType;
   server?: string;
   port?: number;
   username?: string;
   password?: string;
+  uuid?: string;
   raw?: string;
   supportedByTelegram: boolean;
   telegramUrl?: string;
